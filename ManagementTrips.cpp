c5cc80604c496d5f3e99b6b7481b112ca5cd10c6
Shay <135309827+UWotM8Shayy@users.noreply.github.com>
2023-10-19 12:19:26 +0300

    Update ManagementTrips.cpp

--- ManagementTrips.cpp
+++ ManagementTrips.cpp
@@ -1,28 +1,111 @@
-#ifndef MANAGEMENTTRIPS_H
-#define MANAGEMENTTRIPS_H
-
-#include "Trip.h"
 
 
-class ManagementTrips
-{
-private:
-    Trip** trips; 
-    int index; 
+#include "ManagementTrips.h"
+#include <iostream>
 
-public:
-    ManagementTrips();
-    ManagementTrips(const ManagementTrips& other);
 
-    
-    ~ManagementTrips();
+ManagementTrips::ManagementTrips() : trips(nullptr), index(0) {}
 
-    
-    ManagementTrips& operator=(const ManagementTrips& other);
-    ManagementTrips& operator+=(Trip* newTrip);
-    ManagementTrips& operator-=(const Trip* tripToRemove);
-    friend std::ostream& operator<<(std::ostream& os, const ManagementTrips& tripsList);
+ManagementTrips::ManagementTrips(const ManagementTrips& other) : index(other.index) {
+    trips = new Trip * [other.index];
+    for (int i = 0; i < other.index; ++i) {
+        trips[i] = new Trip(*(other.trips[i])); 
+    }
+}
 
-};
 
-#endif 
+ManagementTrips::~ManagementTrips() {
+    if (trips != nullptr) {
+        for (int i = 0; i < index; ++i) {
+            delete trips[i]; 
+        }
+        delete[] trips; 
+    }
+}
+
+ManagementTrips& ManagementTrips::operator=(const ManagementTrips& other) {
+    if (this != &other) {
+        if (trips != nullptr) {
+            for (int i = 0; i < index; ++i) {
+                delete trips[i];
+            }
+            delete[] trips;
+        }
+
+        index = other.index;
+        trips = new Trip * [index];
+        for (int i = 0; i < index; ++i) {
+            trips[i] = new Trip(*(other.trips[i])); 
+        }
+    }
+    return *this;
+}
+
+
+ManagementTrips& ManagementTrips::operator+=(Trip* newTrip) {
+    if (newTrip != nullptr) {
+       
+        Trip** newTrips = new Trip * [index + 1];
+        for (int i = 0; i < index; ++i) {
+            newTrips[i] = trips[i];
+        }
+        newTrips[index] = newTrip; 
+        ++index;
+
+       
+        delete[] trips;
+        trips = newTrips;
+    }
+    return *this;
+}
+
+
+ManagementTrips& ManagementTrips::operator-=(const Trip* tripToRemove) {
+    if (tripToRemove != nullptr) {
+        int removalCount = 0;
+        for (int i = 0; i < index; ++i) {
+            const Date& tripDate = trips[i]->getDate();
+            const Date& removeDate = tripToRemove->getDate();
+            if (tripDate.getDay() == removeDate.getDay() &&
+                tripDate.getMonth() == removeDate.getMonth() &&
+                tripDate.getYear() == removeDate.getYear()) {
+
+                delete trips[i];
+                ++removalCount;
+                trips[i] = nullptr;
+            }
+        }
+
+        
+        Trip** newTrips = new Trip * [index - removalCount];
+        int newIndex = 0;
+        for (int i = 0; i < index; ++i) {
+            if (trips[i] != nullptr) {
+                newTrips[newIndex++] = trips[i];
+            }
+        }
+
+        
+        delete[] trips;
+        trips = newTrips;
+        index = newIndex;
+    }
+    return *this;
+}
+
+
+
+
+std::ostream& operator<<(std::ostream& os, const ManagementTrips& tripsList) {
+    if (tripsList.index == 0) {
+        os << "Trip List Empty";
+    }
+    else {
+        for (int i = 0; i < tripsList.index; ++i) {
+            os << "Trip " << i + 1 << ":" << std::endl;
+            os << *(tripsList.trips[i]); 
+            os << std::endl;
+        }
+    }
+    return os;
+}
