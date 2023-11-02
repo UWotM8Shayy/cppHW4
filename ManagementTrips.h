5c766457d811608613c4f5d7f6daf1c454b5557c
Shay <135309827+UWotM8Shayy@users.noreply.github.com>
2023-11-02 07:57:19 +0200

    Update ManagementTrips.h

--- ManagementTrips.h
+++ ManagementTrips.h
@@ -1,28 +1,23 @@
-#ifndef MANAGEMENTTRIPS_H
-#define MANAGEMENTTRIPS_H
+#ifndef MANAGEMENT_TRIPS_H
+#define MANAGEMENT_TRIPS_H
 
-#include "Trip.h"
+#include "trip.h"
+#include <iostream>
 
-
-class ManagementTrips
-{
-private:
-    Trip** trips; 
-    int index; 
-
-public:
+class ManagementTrips {
+    private:
+        Trip** trips;
+        int index;
+    public:
     ManagementTrips();
     ManagementTrips(const ManagementTrips& other);
-
-    
     ~ManagementTrips();
-
-    
     ManagementTrips& operator=(const ManagementTrips& other);
-    ManagementTrips& operator+=(Trip* newTrip);
-    ManagementTrips& operator-=(const Trip* tripToRemove);
-    friend std::ostream& operator<<(std::ostream& os, const ManagementTrips& tripsList);
+    ManagementTrips& operator+=(Trip& trip);
+    ManagementTrips& operator-=(Trip& trip);
+    friend ostream& operator<<(ostream& os,const ManagementTrips& managment);
+
 
 };
 
-#endif 
+#endif
